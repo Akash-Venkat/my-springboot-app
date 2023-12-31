d4fe017143bc01e2a1fdcb38ca7e46c5b4fba829
Akash Venkatraj <akashvenkat22@gmail.com>
2023-12-31 18:01:21 +0530

    changes

--- src/main/java/com/example/controller/KecController.java
+++ src/main/java/com/example/controller/KecController.java
@@ -1,11 +1,13 @@
 package com.example.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/kec")
 public class KecController {
-    @GetMapping("/kec")
+    @GetMapping(value = "/welcome")
     public String kec() {
         return "welcome to kec";
     }
