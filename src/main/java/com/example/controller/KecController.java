a6973702582d33f4ebfdeb98e4d90b5a722b64b0
Akash Venkatraj <akashvenkat22@gmail.com>
2023-12-31 18:09:10 +0530

    api exposed

--- src/main/java/com/example/controller/KecController.java
+++ src/main/java/com/example/controller/KecController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/kec")
 public class KecController {
-    @GetMapping(value = "/welcome")
+    @GetMapping
     public String kec() {
         return "welcome to kec";
     }
--- src/main/java/com/example/myspringbootapp/MySpringbootAppApplication.java
+++ src/main/java/com/example/myspringbootapp/MySpringbootAppApplication.java
@@ -2,8 +2,10 @@ package com.example.myspringbootapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "com.example")
 public class MySpringbootAppApplication {
 
     public static void main(String[] args) {
